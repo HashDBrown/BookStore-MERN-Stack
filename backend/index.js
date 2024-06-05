8a86b2b956219e2ff4222c550b24d919cf585672
HashDBrown <halkhateeb704@gmail.com>
2024-06-05 15:56:56 -0400

    Get books

--- backend/index.js
+++ backend/index.js
@@ -18,23 +18,36 @@ app.post('/books', async (req, res) => {
         if (!req.body.title || !req.body.author || !req.body.publishYear) {
             return res.status(400).send({ message: 'Please fill all required fields' });
         }
-    
+
         const newBook = {
             title: req.body.title,
             author: req.body.author,
             publishYear: req.body.publishYear,
         };
-        
+
         const book = await Book.create(newBook);
         return res.status(201).send(book);
-    
+
     } catch (error) {
         console.log(error);
         response.status(500).send({ message: error.message });
     }
+});
 
-    
+//Route for getting all books
+app.get('/books', async (req, res) => {
+    try {
+        const books = await Book.find();
 
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        })
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
 });
 
 mongoose
